c5dca1ded16f849b140b1479d6fd863d48aac050
j3nny-zhang <63063216+j3nny-zhang@users.noreply.github.com>
2023-05-26 23:04:18 -0400

    Create kth_largest_in_array.cc

---
+++ priority_queues/kth_largest_in_array.cc
@@ -0,0 +1,38 @@
+// Given an integer array nums and an integer k, return the kth largest element in the array.
+
+// Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+// You must solve it in O(n) time complexity.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,2,1,5,6,4], k = 2
+// Output: 5
+
+class Solution {
+    
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        int i = 0;
+
+        while (k > 0) {
+            pq.push(nums[i++]);
+            k -= 1;
+        }
+
+        for (; i < nums.size(); i++) {
+            int top = pq.top();
+            if (nums[i] > top) {
+                pq.pop();
+                pq.push(nums[i]);
+            }
+        }
+
+        return pq.top();
+        
+    }
+        
+};
