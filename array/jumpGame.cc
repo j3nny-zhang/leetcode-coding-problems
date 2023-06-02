a1f416b4499a35bd3f12df77d5905ba38af28dbd
j3nny-zhang <63063216+j3nny-zhang@users.noreply.github.com>
2023-06-02 14:26:10 -0400

    Create jumpGame.cc

---
+++ array/jumpGame.cc
@@ -0,0 +1,44 @@
+/*
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+*/
+
+class Solution {
+// my first solution is technically correct, but not optimal / time limit exceeded
+  
+//     bool helper(int i, vector<int>& nums) {
+//         if (i == nums.size()-1) return true;
+//         if (i > nums.size()-1) return false;
+//         if (nums[i] == 0) return false;
+
+//         for (int j = 1; j <= nums[i]; j++) {
+//             if (helper(i + j, nums)) return true;
+//         }
+
+//         return false;
+//     }
+
+// public:
+//     bool canJump(vector<int>& nums) {
+        
+//         return helper(0, nums);
+
+//     }
+    public:
+        bool canJump(vector<int>& nums) {
+            int dist = 0;
+            for (int i = 0; i < nums.size(); i++) {
+                if (i == nums.size() - 1) return true;
+                if (nums[i] == 0 && dist == i) return false;
+                dist = max(dist, nums[i] + i);
+            }
+            return true;
+        }
+};
