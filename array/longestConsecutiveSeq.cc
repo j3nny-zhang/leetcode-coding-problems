d5e476af9c0039c70d0121a78e580a1a12aebb13
j3nny-zhang <63063216+j3nny-zhang@users.noreply.github.com>
2023-06-08 19:19:00 -0400

    Create longestConsecutiveSeq.cc

---
+++ array/longestConsecutiveSeq.cc
@@ -0,0 +1,42 @@
+/*
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time.
+
+
+Example 1:
+
+Input: nums = [100,4,200,1,3,2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+*/
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        
+        unordered_map<int, bool> m; // {int, bool} = {value, visited}
+        for (int n : nums) {
+            m[n] = false;
+        }
+
+        int longest, length = 1;
+
+        for (int n : nums) {
+            if (m[n]) continue;
+
+            length = 1;
+            int copy = n;
+            m[n] = true; // have visited the value already
+            while (m.find(copy - 1) != m.end()) {
+                m[copy - 1] = true;
+                length += 1;
+                copy -= 1;
+            }
+            longest = max(longest, length);
+        }
+
+        return longest;
+    }
+};
