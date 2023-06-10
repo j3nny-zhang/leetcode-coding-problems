b6bed77dd7b43039a60f9ef0a45c0da761ed26fc
j3nny-zhang <63063216+j3nny-zhang@users.noreply.github.com>
2023-06-10 13:29:03 -0400

    Update longestConsecutiveSeq.cc

--- array/longestConsecutiveSeq.cc
+++ array/longestConsecutiveSeq.cc
@@ -39,4 +39,30 @@ public:
 
         return longest;
     }
+    
+};
+
+// came up with a more optimal solution, nlogn 
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        
+        sort(nums.begin(), nums.end());
+        
+        int longest = 1;
+        int count = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i-1] == nums[i] - 1) {
+                count += 1;
+            } else if (nums[i-1] == nums[i]) {
+                continue;
+            } else {
+                count = 1;
+            }
+            longest = max(longest, count);
+        }
+
+        return longest;
+    }
 };
