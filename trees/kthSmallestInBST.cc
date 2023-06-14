7b8dea683fd788b67ceeee091f326b3e9ccda6f4
j3nny-zhang <63063216+j3nny-zhang@users.noreply.github.com>
2023-06-14 14:58:29 -0400

    Update kthSmallestInBST.cc

--- trees/kthSmallestInBST.cc
+++ trees/kthSmallestInBST.cc
@@ -28,3 +28,25 @@ public:
         return nodes[k-1];
     }
 };
+
+// this solution does not need to traverse all nodes:
+class Solution {
+    void helper(TreeNode *root, int &k, int& ans) {
+        if (root) {
+            helper(root->left, k, ans);
+            if (--k == 0) {
+                ans = root->val;
+                return;
+            }
+            // nodes.push_back(root->val);
+            helper(root->right, k, ans);
+        }
+    }
+
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        int ans;
+        helper(root, k, ans);
+        return ans;
+    }
+};
