1de46bffbb29178cb330410260dd2a75649271e2
j3nny-zhang <63063216+j3nny-zhang@users.noreply.github.com>
2023-09-23 21:26:59 -0400

    Create min-cost-climbing-stairs.py

---
+++ dynamic_programming/min-cost-climbing-stairs.py
@@ -0,0 +1,34 @@
+"""
+You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+
+You can either start from the step with index 0, or the step with index 1.
+
+Return the minimum cost to reach the top of the floor.
+
+
+Example 1:
+
+Input: cost = [10,15,20]
+Output: 15
+Explanation: You will start at index 1.
+- Pay 15 and climb two steps to reach the top.
+The total cost is 15.
+"""
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        dp = []
+        dp.append(cost[0])
+        dp.append(cost[1])
+
+        # base cases
+        if len(cost) == 2:
+            return min(cost[0], cost[1])
+
+
+        for i in range(2, len(cost)):
+            val = min(dp[i-2]+cost[i], dp[i-1]+cost[i])
+            dp.append(val)
+        
+        
+        return min(dp[-1], dp[-2])
