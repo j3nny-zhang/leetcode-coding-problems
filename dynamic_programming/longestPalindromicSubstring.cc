3593858c78cf020bff01071ebbb3742896a10fbe
j3nny-zhang <63063216+j3nny-zhang@users.noreply.github.com>
2023-10-08 22:15:41 -0400

    Create longestPalindromicSubstring.cc

---
+++ dynamic_programming/longestPalindromicSubstring.cc
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int length = 0;
+        int l = 0;
+        int r = 0;
+
+        if (s.size() == 1) return s;
+
+        // 2d dp array
+        vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), false));
+
+        // base case, single character strings
+        for (int i = 0; i < s.size(); i++) {
+            dp[i][i] = true;
+        }
+
+        for (int i = 1; i < s.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (s[i] != s[j]) continue;
+                if (i == j+1 || dp[i-1][j+1]) {
+                    dp[i][j] = true;
+                    if (length < i - j + 1) {
+                        length = i - j + 1;
+                        r = i;
+                        l = j;
+                    }
+                }
+            }
+        }
+        return s.substr(l, length);
+    }
+};
