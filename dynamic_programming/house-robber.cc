987e035d859cd1089132982102683a6686049f9e
Jenny Zhang <63063216+j3nny-zhang@users.noreply.github.com>
2024-04-21 19:11:13 -0400

    Update house-robber.cc

--- dynamic_programming/house-robber.cc
+++ dynamic_programming/house-robber.cc
@@ -61,3 +61,21 @@ public:
         return curr;
     }
 };
+
+Class Solution { // my most intuitive solution (personally)
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 1) return nums[0];
+        if (nums.size() == 2) return max(nums[0], nums[1]);
+        int n = nums.size();
+        vector<int> dp(n, 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.size(); i++) {
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
+        }
+
+        return dp[n-1];
+    }
+};
