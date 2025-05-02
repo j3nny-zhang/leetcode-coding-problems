6104c41ac2f0d854c16805ad0cc5abacae7726bb
Jenny Zhang <63063216+j3nny-zhang@users.noreply.github.com>
2025-05-02 15:21:02 -0400

    Create subarray-sum-equals-k.cc

---
+++ interview_prep/subarray-sum-equals-k.cc
@@ -0,0 +1,32 @@
+/*
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+*/
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int>  prevSums; // {sum, num of that sum}
+        int currSum = 0;
+
+        int result = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            currSum += nums[i];
+            if (currSum == k) result += 1;
+            if (prevSums.find(currSum - k) != prevSums.end()) result += prevSums[currSum - k];
+            prevSums[currSum] += 1;
+        }
+
+        return result;
+    }
+};
