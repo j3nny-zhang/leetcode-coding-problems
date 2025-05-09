a7377fd2bbd54438a873e0eb64326f505101abfe
Jenny Zhang <63063216+j3nny-zhang@users.noreply.github.com>
2025-05-09 11:30:04 -0400

    Update contains-duplicate-II.cc

--- interview_prep/contains-duplicate-II.cc
+++ interview_prep/contains-duplicate-II.cc
@@ -25,10 +25,8 @@ public:
         for (int i = 0; i < nums.size(); i++) {
             if (m.find(nums[i]) != m.end()) {
                 if (abs(i - m[nums[i]]) <= k) return true;
-                m[nums[i]] = max(m[nums[i]], i);
-            } else {
-                m[nums[i]] = i;
             }
+            m[nums[i]] = i;
         }
         return false;
     }
