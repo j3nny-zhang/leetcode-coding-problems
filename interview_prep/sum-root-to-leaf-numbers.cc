a97aaf31d9a7721c43d72665babd6ea2a666b7b9
Jenny Zhang <63063216+j3nny-zhang@users.noreply.github.com>
2025-05-09 14:28:40 -0400

    Update sum-root-to-leaf-numbers.cc

--- interview_prep/sum-root-to-leaf-numbers.cc
+++ interview_prep/sum-root-to-leaf-numbers.cc
@@ -50,3 +50,23 @@ public:
         return totalSum;
     }
 };
+
+
+// LEETCODE'S SOLN
+class Solution {
+public:
+    void preorder(TreeNode* r, int currNumber, int& rootToLeaf) {
+        if (r != NULL) {
+            currNumber = currNumber * 10 + r->val;
+            // if it's a leaf, update root-to-leaf sum
+            if (r->left == NULL && r->right == NULL) rootToLeaf += currNumber;
+            preorder(r->left, currNumber, rootToLeaf);
+            preorder(r->right, currNumber, rootToLeaf);
+        }
+    }
+    int sumNumbers(TreeNode* root) {
+        int rootToLeaf = 0;
+        preorder(root, 0, rootToLeaf);
+        return rootToLeaf;
+    }
+};
